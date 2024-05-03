bbf81730c92779be4298bd0078daf183b3d55042
rashijais <rashijaiswal2298@gmail.com>
2024-05-03 15:54:04 +0530

    Added Card and Movie List Page

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,11 +1,21 @@
- 
+import { useFetch } from "../hooks/useFetch";
+import { useTitle } from "../hooks/useTitle";
+import { Card } from "../components";
 
-const MovieList = () => {
+
+export const MovieList = ({apiPath, title}) => {
+  const { data: movies } = useFetch(apiPath);
+  useTitle(title);
+
   return (
     <main>
-       <div></div>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap other:justify-evenly">       
+          { movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          )) }          
+        </div>
+      </section>
     </main>
   )
 }
-
-export default MovieList
--- src/routes/Allroutes.jsx
+++ src/routes/Allroutes.jsx
@@ -1,25 +1,18 @@
-import { Routes,Route } from "react-router-dom";
-import MovieDetail from "../pages/MovieDetail";
-import PageNotFound from "../pages/PageNotFound";
-import Search from "../pages/Search";
-import MovieList from "../pages/MovieList";
+import { Routes, Route } from "react-router-dom";
+import { MovieList, MovieDetail, Search, PageNotFound } from "../pages";
 
-
-const Allroutes = () => {
+export const AllRoutes = () => {
   return (
-    <>
+    <div className="dark:bg-darkbg">
         <Routes>
-            <Route path="/" element={<MovieList/>}/>
-            <Route path="movie/:id" element={<MovieDetail/>}/>
-            <Route path="movies/popular" element={<MovieList/>}/>
-            <Route path="movies/top" element={<MovieList/>}/>
-            <Route path="movies/upcoming" element={<MovieList/>}/>
-            <Route path="search" element={<Search/>}/>
-            <Route path="*" element={<PageNotFound/>}/>
+            <Route path="" element={<MovieList apiPath="movie/now_playing" title="Home" />} />
+            <Route path="movie/:id" element={<MovieDetail />} />
+            <Route path="movies/popular" element={<MovieList apiPath="movie/popular" title="Popular" />} />
+            <Route path="movies/top" element={<MovieList apiPath="movie/top_rated" title="Top Rated" />} />
+            <Route path="movies/upcoming" element={<MovieList apiPath="movie/upcoming" title="Upcoming" />} />
+            <Route path="search" element={<Search apiPath="search/movie" />} />
+            <Route path="*" element={<PageNotFound />} />
         </Routes>
-      
-    </>
+    </div>
   )
 }
-
-export default Allroutes;
